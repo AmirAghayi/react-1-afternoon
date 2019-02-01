c2e754d01c9e8c02f31ffc12970f91c7caf4cb7d
Amir aghayi_chadegani <amiraghayi_chadegani@Amirs-iMac.local>
2019-02-01 12:32:58 -0700

    afternoon-1-react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+import EvenAndOdd from './components/Topics/EvenAndOdd';
+import filterObject from './components/Topics/FilterObject';
+import FilterString from './components/Topics/FilterString';
+import Palindrome from './components/Topics/Palindrome';
+import Sum from './components/Topics/Sum';
+import FilterObject from './components/Topics/FilterObject';
+
 
 class App extends Component {
   render() {
-    
+    return(
+      <div>
+        <TopicBrowser />
+        <EvenAndOdd />
+        <FilterObject />
+        <FilterString />
+        <Palindrome />
+        <Sum />
+      </div>
+    )
   }
 }
 
